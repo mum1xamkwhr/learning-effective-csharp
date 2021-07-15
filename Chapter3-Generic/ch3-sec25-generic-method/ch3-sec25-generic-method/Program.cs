95156175568ad0510b7b6f104fd98bf70f6743dd
mum1xamkwhr <86910595+mum1xamkwhr@users.noreply.github.com>
2021-07-15 18:01:44 +0900

    【sample】項目25 型引数がインスタンスのフィールドではない場合にはジェネリックメソッドとして定義すること

---
+++ Chapter3-Generic/ch3-sec25-generic-method/ch3-sec25-generic-method/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ch3_sec25_generic_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            var listFirst = new List<string>() { "a", "b", "C" };
+            var listSecond = new List<string>() { "D", "e", "最後は日本語" };
+            var comma = new CommaSeparatedListBuilder();
+
+            comma.Add(listFirst);
+            comma.Add(listSecond);
+            Console.WriteLine(comma.ToString());
+        }
+    }
+}
