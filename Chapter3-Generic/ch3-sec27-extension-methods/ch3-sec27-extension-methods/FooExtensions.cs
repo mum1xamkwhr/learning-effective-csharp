998281b2db7b4f64d055381c3a04dfc654fefe56
mum1xamkwhr <86910595+mum1xamkwhr@users.noreply.github.com>
2021-07-27 09:17:44 +0900

    【sample】項目27 最小限に制限されたインターフェースを拡張メソッドにより機能拡張する

---
+++ Chapter3-Generic/ch3-sec27-extension-methods/ch3-sec27-extension-methods/FooExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ch3_sec27_extension_methods
+{
+    public static class FooExtensions
+    {
+        public static void NextMarker(this IFoo thing)
+        {
+            thing.Marker += 1;
+        }
+    }
+}
---
+++ Chapter3-Generic/ch3-sec27-extension-methods/ch3-sec27-extension-methods/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ch3_sec27_extension_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            var t = new MyType();
+            t.UpdateMarker();
+            Console.WriteLine(t.Marker.ToString());
+
+            var t2 = new MyType2();
+            t2.UpdateMarker();
+            Console.WriteLine(t2.Marker.ToString());
+        }
+
+        class MyType : IFoo
+        {
+            public int Marker { get; set; }
+            public void UpdateMarker() => this.NextMarker();
+        }
+
+        class MyType2 : IFoo
+        {
+            public int Marker { get; set; }
+            public void NextMarker() => Marker += 5;
+            public void UpdateMarker() => this.NextMarker();
+        }
+    }
+}
